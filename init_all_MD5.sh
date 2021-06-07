dc02b9adb99b28dda9a5f82b3a1045eb746dbf11
Mejux06 <56360412+Mejux06@users.noreply.github.com>
2021-06-07 14:36:38 +0800

    Add files via upload

---
+++ init_all_MD5.sh
@@ -0,0 +1,7 @@
+    init_data_ID=init_data.sh
+    init_data_MD5=e63941ef3956fcd9513394a0ca8f9d4b
+    Util_Functions_ID=Util_Functions.sh
+    Util_Functions_MD5=6390e0a60d5c772766e2bbc69f306f27
+    Cloud_ID=Cloud_Page.zip
+    Cloud_MD5=89626837cb2e07d3c7cbabf4a401bd50
+    Cloud_Version=1
